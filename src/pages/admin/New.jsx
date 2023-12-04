69a2b2f70d1cbdb8a154352b512da26f94ce1df6
Mohammad Heidary <mohammad.heidary.extern@jsmd-group.com>
2023-12-04 21:52:22 +0330

    add alert for last years fields

--- src/pages/admin/New.jsx
+++ src/pages/admin/New.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
   Box,
-  Container, HStack, Heading, Image,
+  Button,
+  Checkbox,
+  Container, HStack, Heading, Image, useDisclosure,
 } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 
-export default function Dashboard() {
+export default function New() {
+  const [dontShowAgain, setDontShowAgain] = useState(false);
+  const shouldShowModal = !dontShowAgain && !localStorage.getItem('dontShowAgain');
+  const { isOpen, onClose } = useDisclosure({ defaultIsOpen: shouldShowModal });
+
+  const handleClose = () => {
+    if (dontShowAgain) {
+      localStorage.setItem('dontShowAgain', 'true');
+    }
+
+    onClose();
+  };
+
   return (
     <Container justifyItems="center">
       <Heading textAlign="center" marginTop={10}>Choose a Crop Type:</Heading>
@@ -44,6 +64,41 @@ export default function Dashboard() {
           <Heading textAlign="center" margin={5} color="white">Soybean</Heading>
         </Box>
       </HStack>
+
+      <AlertDialog
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Info
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              If you already have a field from last year, you can use it for this year!
+              <br />
+              <Checkbox
+                marginTop={5}
+                isChecked={dontShowAgain}
+                onChange={(e) => setDontShowAgain(e.target.checked)}
+              >
+                Don&lsquo;t show again
+              </Checkbox>
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button as={NavLink} to="/admin/" onClick={onClose}>
+                Go back!
+              </Button>
+              <Button colorScheme="blue" onClick={handleClose} ml={3}>
+                Got it!
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </Container>
   );
 }
